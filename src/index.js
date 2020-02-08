afa9f14a0b662470bec18a00c9ddb03d18b3ab50
Saša Fazlić <fazlic.sasa@gmail.com>
2020-02-08 21:25:42 +0100

    connected app to redux store

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import { render } from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import { ThemeProvider } from "emotion-theming"
 import { theme } from "./theme"
+import { Provider } from "react-redux"
+import store from "./redux/store"
 import App from "./App"
 
 render(
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("App")
 )
 
